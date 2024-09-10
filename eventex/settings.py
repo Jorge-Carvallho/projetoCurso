d20f0133e12d1fbd37ed7787790c69ab51a261d6
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-09-09 21:43:55 -0300

    Adicionando congiguração de DBUG e CHAVE_KEY

--- eventex/settings.py
+++ eventex/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-d))mcjk8uml7c9c5-j@-zhqoor+h9&m1+y_-r#b%#e-4ojyz)v'
-SECRER_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-d))mcjk8uml7c9c5-j@-zhqoor+h9&m1+y_-r#b%#e-4ojyz)v'
+# SECRER_KEY = config('SECRET_KEY')
 
 # # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
